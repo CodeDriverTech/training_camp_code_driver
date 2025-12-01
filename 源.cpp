178c324b3566b29f0b84a1bd01e1f6190d5043a7
nanmingbeichen <859423459@qq.com>
2025-12-01 22:13:23 +0800

    do while

---
+++ 源.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	do
+//		printf("%d", i);
+//	while (i <= 10);       //do while 循环
+//		return 0;          //打印出来的结果是无数个 1
+//}
+
+int main()
+{
+	int a = 2;
+	do 
+	{
+		printf("% d", a);
+		a++;
+	}                                //do  while 循环打印小于等于20的一系列数字
+		while (a <= 20);
+		return 0;
+}
