68cc41ee5dd9a2da4d9484f45ed982f3b8d36a68
LiYu <1745730759@qq.com>
2025-12-14 20:31:48 +0800

    java

---
+++ 软件开发/HuJichao/eq.java
@@ -0,0 +1,18 @@
+public class eq {
+    public static void main(String[] args) {
+        //创建两个字符串对象
+        String s1 =  new String("abc");
+        String s2 = "abc";
+
+        //2==比较
+        //基本数据比较的是值
+        /*引用数据类型比较的是地址值*/
+        System.out.println(s1==s2);
+
+    }
+
+
+
+
+
+}
